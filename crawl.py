1e81875fd5f5b791e633a7dd60df27d10994e802
illmet <ilyaamet@gmail.com>
2024-02-25 11:56:38 +0000

    minor improvements

--- crawl.py
+++ crawl.py
@@ -7,11 +7,10 @@ import requests
 
 ###CONFIG
 #the time period for the scraping
-#CHANGE THESE TO BE UNIX EPOCHS FROM THE START
 campaign_end = int(datetime(2020, 11, 3).timestamp()) #election day
 campaign_start = int(datetime(2020, 8, 18).timestamp()) #biden nomination by dnc
-print(f"End: {campaign_end}")
-print(f"Start: {campaign_start}")
+#print(f"End: {campaign_end}")
+#print(f"Start: {campaign_start}")
 
 #enviroments for connecting to reddit 
 ci = os.getenv('cireddit')
@@ -26,25 +25,22 @@ reddit = praw.Reddit(
     user_agent=ua
 )
 #dir
-output_dir = "data"
-#separate lists for titles, posts and comments
-titles = []
-posts = []
-comments = []
-#comment counter, but I had it in the wrong place
-#comm = 1
+output = "data"
+postsdir = os.path.join(output, "posts")
+commsdir = os.path.join(output, "comments")
 #+++limit the number of posts
+
 ###SCRAPE
 def scrape_reddit_json(subreddit):
     n = 0
     fullposts = []
-    #fullcomms = []
-    for p in reddit.subreddit(subreddit).search('"Biden" OR "Trump"', time_filter="all"):
-    #for p in reddit.subreddit(subreddit).new(limit=10):
-        time = p.created_utc
-        g = int(p.created_utc)
-        print(time)
-        if campaign_start <= g <= campaign_end:
+    fullcomms = []
+    #the search string is annoying, I haven't figured out the way to do joins properly...
+    #for now, only gets the mention of either Biden or Trump in the specified period
+    for p in reddit.subreddit(subreddit).search('Trump', time_filter="all"):
+        time = int(p.created_utc)
+        #print(time)
+        if campaign_start <= time <= campaign_end:
             fullposts.append({
                 "id": p.id,
                 "title": p.title,
@@ -56,30 +52,37 @@ def scrape_reddit_json(subreddit):
                 "date": time
             })
             
-            #p.comments.replace_more(limit=10)
-            #for comment in p.comments.list():
-                #fullcomms.append({
-                    #"body": comment.body,
-                    #"upvotes": comment.score,
-                    #"author": str(comment.author) if comment.author else None,
-                    #"date": datetime.fromtimestamp(comment.created_utc),
-                    #"parent_id": comment.parent_id
-                #})
+            p.comments.replace_more(limit=10)
+            for comment in p.comments.list():
+                fullcomms.append({
+                    "body": comment.body,
+                    "upvotes": comment.score,
+                    "author": str(comment.author) if comment.author else None,
+                    "date": datetime.fromtimestamp(comment.created_utc),
+                    "post_id": p.id,
+                    "parent_id": comment.parent_id
+                })
             
             #full.append(info)
             print(f"Post {n} crawled")
             n+=1
-    return fullposts
+    return fullposts, fullcomms
+    
 ###FILE SAVE
 for sub in subs:
-    info = scrape_reddit_json(sub)
+    posts, comments = scrape_reddit_json(sub)
     #print(info)
-    filename = f"{sub}.json"
-    filepath = os.path.join(output_dir, filename)
-    with open(filepath, 'w') as f:
-        json.dump(info, f, indent=4) 
+    #save posts
+    pathpost = os.path.join(postsdir, f"{sub}_posts.json")
+    with open(pathpost, 'w') as f:
+        json.dump(posts, f, indent=4) 
+    #save comment
+    pathcomm = os.path.join(commsdir, f"{sub}_comments.json")
+    with open(pathcomm, 'w') as f:
+        json.dump(comms, f, indent=4)
+    
 
-#refactored txt file writing method (optional, obsolete)
+#refactored txt file writing method (optional, obsolete), do not uncomment, do not delete! thanks.
 #def scrape_reddit_txt():
     #with open(file_name, "w") as f:
         #for post in reddit.subreddit(subreddit).submissions(campaign_start.timestamp(), campaign_end.timestamp()):
