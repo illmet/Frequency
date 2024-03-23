897ad01ee306fe3f1d5438365611e516820feeb3
Chiara D'Ercoli <dercolic@tcd.ie>
2024-03-23 18:47:17 +0000

    Added stopwords removal and tokenization with nltk and spaCy

--- main.py
+++ main.py
@@ -1,8 +1,41 @@
 from processing import process_data
+import data
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+import spacy
 
 def get_data():
     posts = process_data("data/posts", "content")
     comms = process_data("data/comments", "body")
-    al = posts + comms
-    return al
+    all = posts + comms
+    return " ".join(all)
 
+
+
+nltk.download('stopwords')
+nltk.download('punkt')
+
+#spaCy model to be downloaded
+# !python -m spacy download en_core_web_sm
+
+nlp = spacy.load("en_core_web_sm")
+
+# Increase max_length to accommodate longer texts
+nlp.max_length = 2000000000000
+
+def remove_stopwords_nltk(text):
+    stop_words = set(stopwords.words('english'))
+    word_tokens = word_tokenize(text)
+    filtered_text = [w for w in word_tokens if w.lower() not in stop_words]
+    return ' '.join(filtered_text)
+
+def remove_stopwords_spacy(text):
+    doc = nlp(text)
+    filtered_text = [token.text for token in doc if not token.is_stop]
+    return ' '.join(filtered_text)
+
+text = get_data()
+
+nltk_data = remove_stopwords_nltk(text)
+spacy_data = remove_stopwords_spacy(text)
